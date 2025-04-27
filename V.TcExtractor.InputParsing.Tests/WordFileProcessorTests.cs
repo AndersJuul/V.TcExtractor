243ad721b31378b4c1f4d9cae010e100fba077a3
AJU2024\aju <andersjuulsfirma@gmail.com>
2025-04-27 11:43:30 +0200

    Added Handle_returns_testcases_for_psi_dvpr_ves_pv_interface

--- V.TcExtractor.InputParsing.Tests/WordFileProcessorTests.cs
+++ V.TcExtractor.InputParsing.Tests/WordFileProcessorTests.cs
@@ -216,6 +216,26 @@ namespace V.TcExtractor.InputParsing.Tests
             Assert.All(testCases, x => Assert.False(string.IsNullOrEmpty(x.Description)));
         }
 
+        [Fact]
+        public void Handle_returns_testcases_for_psi_dvpr_ves_pv_interface()
+        {
+            // Arrange
+            var sut = GetSut();
+
+            // Act
+            var testCases = sut
+                .Handle(Path.Combine(TestDataBasePath, "PSI", "DVPR VES PV Interface.docx"))
+                .ToArray();
+
+            // Assert
+            Dump(testCases);
+            Assert.Equal(2, testCases.Count());
+
+            Assert.All(testCases, x => Assert.False(string.IsNullOrEmpty(x.TestNo)));
+            Assert.All(testCases, x => Assert.False(string.IsNullOrEmpty(x.FileName)));
+            Assert.All(testCases, x => Assert.False(string.IsNullOrEmpty(x.Description)));
+        }
+
         private static WordFileProcessor GetSut()
         {
             var cellAdapter = new CellAdapter();
