03fd06bcd02c7180dda23ba526e71c2445c81aba
AJU2024\aju <andersjuulsfirma@gmail.com>
2025-04-27 11:35:39 +0200

    Added Handle_returns_testcases_for_psi_dvpr_ves_power_meter

--- V.TcExtractor.InputParsing.Tests/WordFileProcessorTests.cs
+++ V.TcExtractor.InputParsing.Tests/WordFileProcessorTests.cs
@@ -169,6 +169,26 @@ namespace V.TcExtractor.InputParsing.Tests
             Assert.All(testCases, x => Assert.False(string.IsNullOrEmpty(x.Description)));
         }
 
+        [Fact]
+        public void Handle_returns_testcases_for_psi_dvpr_ves_power_meter()
+        {
+            // Arrange
+            var sut = GetSut();
+
+            // Act
+            var testCases = sut
+                .Handle(Path.Combine(TestDataBasePath, "PSI", "DVPR VES Power Meter.docx"))
+                .ToArray();
+
+            // Assert
+            Dump(testCases);
+            Assert.Equal(11, testCases.Count());
+
+            Assert.All(testCases, x => Assert.False(string.IsNullOrEmpty(x.TestNo)));
+            Assert.All(testCases, x => Assert.False(string.IsNullOrEmpty(x.FileName)));
+            Assert.All(testCases, x => Assert.False(string.IsNullOrEmpty(x.Description)));
+        }
+
         private static WordFileProcessor GetSut()
         {
             var cellAdapter = new CellAdapter();
