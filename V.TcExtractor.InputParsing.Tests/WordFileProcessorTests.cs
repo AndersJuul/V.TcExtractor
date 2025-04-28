fd3d45acc1100124b6dc34c801ec8d358c13d302
AJU2024\aju <andersjuulsfirma@gmail.com>
2025-04-28 09:08:24 +0200

    Added Handle_returns_testcases_for_spc_dvpr_ves_vot

--- V.TcExtractor.InputParsing.Tests/WordFileProcessorTests.cs
+++ V.TcExtractor.InputParsing.Tests/WordFileProcessorTests.cs
@@ -376,6 +376,26 @@ namespace V.TcExtractor.InputParsing.Tests
             Assert.All(testCases, x => Assert.False(string.IsNullOrEmpty(x.Description)));
         }
 
+        [Fact]
+        public void Handle_returns_testcases_for_spc_dvpr_ves_vot()
+        {
+            // Arrange
+            var sut = GetSut();
+
+            // Act
+            var testCases = sut
+                .Handle(Path.Combine(TestDataBasePath, "SPC", "DVPR VES - VOT.docx"))
+                .ToArray();
+
+            // Assert
+            Dump(testCases);
+            Assert.Equal(36, testCases.Count());
+
+            Assert.All(testCases, x => Assert.False(string.IsNullOrEmpty(x.TestNo)));
+            Assert.All(testCases, x => Assert.False(string.IsNullOrEmpty(x.FileName)));
+            Assert.All(testCases, x => Assert.False(string.IsNullOrEmpty(x.Description)));
+        }
+
         private static WordFileProcessor GetSut()
         {
             var sut = GetWordFileProcessor();
