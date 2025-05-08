463f5489e018fa72b3106fac98a0ddced37499f8
AJU2024\aju <andersjuulsfirma@gmail.com>
2025-05-08 10:41:10 +0200

    Fixed SPN/SPC DVPL issue

--- V.TcExtractor.Infrastructure.OfficeDocuments/Adapters/FileAdapters/SpcDvplFileProcessor.cs
+++ V.TcExtractor.Infrastructure.OfficeDocuments/Adapters/FileAdapters/SpcDvplFileProcessor.cs
@@ -8,8 +8,8 @@ public class SpcDvplFileProcessor : IDvplFileProcessor
     public bool CanHandle(string fileName)
     {
         var nameWithoutPath = Path.GetFileName(fileName);
-        return
-            false; //nameWithoutPath.Contains("DVPL") && (nameWithoutPath.Contains("SPN") || nameWithoutPath.Contains("SPC"));
+        return nameWithoutPath.Contains("DVPL")
+               && (nameWithoutPath.Contains("SPN") || nameWithoutPath.Contains("SPC"));
     }
 
     public IEnumerable<DvplItem> GetDvplItems(string fileName)
@@ -21,9 +21,9 @@ public class SpcDvplFileProcessor : IDvplFileProcessor
 
             foreach (var row in range.Rows().Skip(3))
             {
-                var productRsCode = "";
-                var moduleRsCode = row.Cell(8).GetString();
-                var testLocation = row.Cell(27).GetString();
+                var productRsCode = row.Cell(6).GetString();
+                var moduleRsCode = row.Cell(3).GetString();
+                var testLocation = row.Cell(17).GetString();
                 yield return new DvplItem
                 {
                     ProductRsCode = productRsCode,
